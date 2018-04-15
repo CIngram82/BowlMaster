4e183625d37f1c542b3e84f0e76f730cd6489432
CIngram82 <cingram82@gmail.com>
2018-04-15 11:06:12 -0400

    Testing first frames of game

--- Assets/Editor/ActionMasterTest.cs
+++ Assets/Editor/ActionMasterTest.cs
@@ -6,18 +6,53 @@ using NUnit.Framework;
 [TestFixture]
 public class ActionMasterTest
 {
+    private ActionMaster actionMaster = new ActionMaster();
     private ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
-    private ActionMaster actionMaster;
+    private ActionMaster.Action reset = ActionMaster.Action.Reset;
+    private ActionMaster.Action tidy = ActionMaster.Action.Tidy;
+
+
+
     [Test]
     public void T00PassingTest()
     {
         Assert.AreEqual(1, 1);
     }
-
     [Test]
-    public void T01OneStrikeReturnEndTurn()
+    public void T01Bowl8ReturnsTidy()
     {
-        ActionMaster actionMaster = new ActionMaster();
+        Assert.AreEqual(tidy, actionMaster.Bowl(8));
+    }
+    [Test]
+    public void T02CheckScoreofFirstBowlReturns8()
+    {
+        Assert.AreEqual(8, actionMaster.bowls[0]);
+    }
+    [Test]
+    public void T03EndOfFrameReturnsEndTurn()
+    {
+        Assert.AreEqual(endTurn, actionMaster.Bowl(1));
+    }
+    [Test]
+    public void T04CheckScoreAfterFirstFrameReturns9()
+    {
+        Assert.AreEqual(9, actionMaster.frames[0]);
+    }
+    [Test]
+    public void T05OneStrikeReturnEndTurn()
+    {
         Assert.AreEqual(endTurn, actionMaster.Bowl(10));
+    }
+    [Test]
+    public void T06Bowl28SpareReturnsEndTurn()
+    {
+        Assert.AreEqual(tidy,    actionMaster.Bowl(2));
+        Assert.AreEqual(endTurn, actionMaster.Bowl(8));
+    }
+    [Test]
+   
+    public void T07CheckScoreof()
+    {
+
     }
 }
--- Assets/Scrpits/ActionMaster.cs
+++ Assets/Scrpits/ActionMaster.cs
@@ -5,12 +5,29 @@ using UnityEngine;
 public class ActionMaster {
     public enum Action { Tidy, Reset, EndTurn, Endgame}
 
+    public int[] bowls = new int[21];
+    public int[] frames = new int[10];
+    private int ballThrow = 1;
+
     public Action Bowl(int pins)
     {
         if(pins < 0 || pins > 10) throw new UnityException("Invalid Number of pins");
-        
-        if (pins == 10)
+        bowls[ballThrow - 1] = pins;
+        if (pins == 10) // strike
         {
+            frames[ballThrow / 2] = 10;
+            ballThrow += 2;
+            return Action.EndTurn;
+        }
+
+        if (ballThrow % 2 != 0) //first throw of the frame
+        {
+            ballThrow++;
+            return Action.Tidy;
+        } else if (ballThrow % 2 == 0) //2nd throw of the frame
+        {
+            frames[(ballThrow / 2)-1] = pins + bowls[ballThrow-2];
+            ballThrow++;
             return Action.EndTurn;
         }
 
