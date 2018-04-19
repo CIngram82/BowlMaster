42106c84ccb8bee1acad041c28861999502ec26e
CIngram82 <cingram82@gmail.com>
2018-04-19 16:15:02 -0400

    ScoreMasterTest Passed
    Passed all Tests and Done Refactoring on ScoreMaster.

--- Assets/Scrpits/ScoreMaster.cs
+++ Assets/Scrpits/ScoreMaster.cs
@@ -1,15 +1,43 @@
-﻿using System.Collections;
+﻿using System.Linq;
 using System.Collections.Generic;
-using UnityEngine;
 
 public class ScoreMaster {
-    
+
+    public static List<int> ScoreCumulative(List<int> rolls)
+    {
+        List<int> CumulativeScores = new List<int>();
+        int runningTotal = 0;
+        foreach (int frameScore in ScoreFrames(rolls))
+        {
+            runningTotal += frameScore;
+            CumulativeScores.Add(runningTotal);
+        }
+        return CumulativeScores;
+    }
+
     public static List<int> ScoreFrames(List<int> rolls)
     {
         List<int> frameList = new List<int>();
-        // Your code here
-
+        int frameTotal = 0;
+        for (int i = 0,bowlCount = 0 ; i < rolls.Count - 1; i+=2, bowlCount+=2 )
+        {
+            if (bowlCount >= 18 && rolls[i] == 10){
+                frameList.Add(10 + rolls[i + 1] + rolls[i + 2]);
+                return frameList;
+            }else if (rolls[i] == 10){
+                if(rolls.Count > i + 2){
+                    frameList.Add(10 + rolls[i + 1] +rolls[i+2]);
+                    i--;
+                }
+            }else if ((rolls[i]+rolls[i+1])==10){
+                if (rolls.Count > i + 2){
+                    frameList.Add(10 + rolls[i + 2]);
+                }
+            }else{
+                frameTotal = rolls[i] + rolls[i + 1];
+                frameList.Add(frameTotal);
+            }
+        }  
         return frameList;
     }
-
 }
